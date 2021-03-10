c703aacd839e11c79b9c6b467a20a9b2bb5f6e02
Boris <rozumniy.boris.work@gmail.com>
2021-03-10 15:30:55 +0200

    create the Global style

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import { Global } from './styles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Global />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
