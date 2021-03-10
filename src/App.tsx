0dacf14067a1196044e8c3afeed5655cc84b33c4
Boris <rozumniy.boris.work@gmail.com>
2021-03-10 15:32:57 +0200

    create the FirstScreen component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Component } from './components/FirstScreen';
 
 export const App = () => {
   return (
-    <H1>
-      replication of <a href="https://www.etecc.com/">https://www.etecc.com/</a>
-    </H1>
+    <>
+      <Component />
+    </>
   );
 };
-
-const H1 = styled.h1`
-  color: red;
-  background: #dde;
-  padding: 14px;
-`;
---
+++ src/components/FirstScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Component = () => <H1>Component</H1>;
+
+const H1 = styled.h1`
+  color: red;
+  background: #dde;
+  padding: 14px;
+`;
