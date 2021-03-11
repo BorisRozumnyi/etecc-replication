5dede66f12a119952e387aa59e56071b18c57dc6
Boris <rozumniy.boris.work@gmail.com>
2021-03-11 15:25:01 +0200

    move the H1 to the styles

--- src/components/FirstScreen.tsx
+++ src/components/FirstScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Flex } from 'src/styles';
+import { Container, Flex, H1 } from 'src/styles';
 import styled from 'styled-components';
 
 export const FirstScreen = () => (
@@ -20,62 +20,6 @@ const Wrapper = styled(Flex)`
   height: 35.625rem;
   position: relative;
   overflow: hidden;
-`;
-
-const H1 = styled.h1`
-  font-size: 2.75rem;
-  @media (min-width: 768px) {
-    font-size: 3.75rem;
-  }
-  line-height: 2.95rem;
-  letter-spacing: 0;
-  font-family: 'Rozha One', serif;
-  color: #fff;
-  text-align: center;
-  &:before,
-  &:after {
-    transform: rotateZ(-45deg);
-    width: 100%;
-    position: absolute;
-    content: '';
-    background-color: rgba(255, 255, 255, 0.7);
-    height: 1px;
-    z-index: 0;
-  }
-  :before {
-    right: -60%;
-    top: -40px;
-    @media (min-width: 768px) {
-      right: -40%;
-      top: -170px;
-    }
-    @media (min-width: 992px) {
-      top: -260px;
-    }
-    @media (min-width: 1024px) {
-      top: -330px;
-    }
-    @media (min-width: 1440px) {
-      right: 40px;
-    }
-  }
-  :after {
-    left: -60%;
-    bottom: -40px;
-    @media (min-width: 768px) {
-      left: -40%;
-      bottom: -170px;
-    }
-    @media (min-width: 992px) {
-      bottom: -260px;
-    }
-    @media (min-width: 1024px) {
-      bottom: -330px;
-    }
-    @media (min-width: 1440px) {
-      left: 40px;
-    }
-  }
 `;
 
 const H3 = styled.h3`
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -29,6 +29,62 @@ export const Flex = styled.div`
   padding: 0 15px;
 `;
 
+export const H1 = styled.h1`
+  font-size: 2.75rem;
+  @media (min-width: 768px) {
+    font-size: 3.75rem;
+  }
+  line-height: 2.95rem;
+  letter-spacing: 0;
+  font-family: 'Rozha One', serif;
+  color: #fff;
+  text-align: center;
+  &:before,
+  &:after {
+    transform: rotateZ(-45deg);
+    width: 100%;
+    position: absolute;
+    content: '';
+    background-color: rgba(255, 255, 255, 0.7);
+    height: 1px;
+    z-index: 0;
+  }
+  :before {
+    right: -60%;
+    top: -40px;
+    @media (min-width: 768px) {
+      right: -40%;
+      top: -170px;
+    }
+    @media (min-width: 992px) {
+      top: -260px;
+    }
+    @media (min-width: 1024px) {
+      top: -330px;
+    }
+    @media (min-width: 1440px) {
+      right: 40px;
+    }
+  }
+  :after {
+    left: -60%;
+    bottom: -40px;
+    @media (min-width: 768px) {
+      left: -40%;
+      bottom: -170px;
+    }
+    @media (min-width: 992px) {
+      bottom: -260px;
+    }
+    @media (min-width: 1024px) {
+      bottom: -330px;
+    }
+    @media (min-width: 1440px) {
+      left: 40px;
+    }
+  }
+`;
+
 type H2Props = {
   mt?: number;
 };
