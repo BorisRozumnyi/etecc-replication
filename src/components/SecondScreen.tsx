a4700fa7a092fdd3c73612c7e6c0eeadc3679a65
Boris <rozumniy.boris.work@gmail.com>
2021-03-11 14:37:54 +0200

    move the H2 to the styles

--- src/components/SecondScreen.tsx
+++ src/components/SecondScreen.tsx
@@ -1,43 +1,8 @@
 import React from 'react';
-import { Flex } from 'src/styles';
-import styled from 'styled-components';
+import { Flex, H2 } from 'src/styles';
 
 export const SecondScreen = () => (
   <Flex>
-    <H2>
-      <Span>HELLO</Span>
-    </H2>
+    <H2>HELLO</H2>
   </Flex>
 );
-
-const H2 = styled.h2`
-  width: 100%;
-  margin-bottom: 10px;
-  text-align: center;
-  font-size: 1.5rem;
-  line-height: 1.55;
-  letter-spacing: 2px;
-  font-family: 'PantReg', Helvetica, sans-serif;
-  overflow: hidden;
-`;
-
-const Span = styled.span`
-  position: relative;
-  display: inline-block;
-  &:before {
-    content: '';
-    position: absolute;
-    top: 50%;
-    border-bottom: 1px solid #802121;
-    width: 591px;
-    margin: 0 20px;
-  }
-  &:after {
-    content: '';
-    position: absolute;
-    top: 50%;
-    border-bottom: 1px solid #e9e9e9;
-    width: 591px;
-    margin: 0 20px;
-  }
-`;
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -15,3 +15,35 @@ export const Flex = styled.div`
   align-items: center;
   padding: 0 15px;
 `;
+
+export const H2 = styled.h2`
+  width: 100%;
+  margin-bottom: 10px;
+  text-align: center;
+  font-size: 1.5rem;
+  line-height: 1.55;
+  letter-spacing: 2px;
+  font-family: 'PantReg', Helvetica, sans-serif;
+  overflow: hidden;
+
+  &:before,
+  &:after {
+    background-color: #e9e9e9;
+    content: '';
+    display: inline-block;
+    height: 1px;
+    position: relative;
+    vertical-align: middle;
+    width: 50%;
+  }
+
+  &:before {
+    right: 0.5em;
+    margin-left: -50%;
+  }
+
+  &:after {
+    left: 0.5em;
+    margin-right: -50%;
+  }
+`;
