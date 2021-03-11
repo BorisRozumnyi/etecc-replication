6d9a10675fa0b339d4282f21b3184160fc3278ee
Boris <rozumniy.boris.work@gmail.com>
2021-03-11 13:39:33 +0200

    create the SecondScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { FirstScreen } from './components/FirstScreen';
+import { SecondScreen } from './components/SecondScreen';
 
 export const App = () => {
   return (
     <>
       <FirstScreen />
+      <SecondScreen />
     </>
   );
 };
